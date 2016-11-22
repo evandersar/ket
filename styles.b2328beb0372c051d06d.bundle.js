ae88fd8dadaedea776de807622ada272a1142e0e
ivan zinchenko <evandersar@ukr.net>
2016-11-22 17:33:47 +0200

    new gh-pages version

--- styles.b2328beb0372c051d06d.bundle.js
+++ styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{330:function(n,o){},599:function(n,o,c){n.exports=c(330)}},[599]);
+webpackJsonp([1,2],{374:function(n,o){},673:function(n,o,c){n.exports=c(374)}},[673]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
