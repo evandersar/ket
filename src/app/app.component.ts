414af5ce7a5e0c96ea8ef2083fc3747ca88eadb3
ivan zinchenko <evandersar@ukr.net>
2016-11-22 17:29:08 +0200

    all in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PageScrollConfig } from "ng2-page-scroll";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(){
+    PageScrollConfig.defaultScrollOffset = 50;
+    PageScrollConfig.defaultDuration = 1000;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ServicesComponent } from './services/services.component';
+import { SliderComponent } from './slider/slider.component';
+
+import { Ng2PageScrollModule } from "ng2-page-scroll";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    FormComponent,
+    NavigationComponent,
+    ServicesComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    Ng2PageScrollModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
