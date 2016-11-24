3fce1281c8cc3d74b3ec99c70d9df25460f53e1a
ivan zinchenko <evandersar@ukr.net>
2016-11-24 16:32:16 +0200

    portfolio hover implemented

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,12 +11,25 @@ export class PortfolioComponent implements OnInit {
 
   category: string = 'All';
   images = images;
+  /*isHidden: boolean = true;
+  hid: string = 'hidden';*/
 
   catChanger(cat){
     this.category = cat;
     //console.log(this.images);
   }
 
+  show(e){
+    /*this.isHidden = false;
+    this.hid ='visible';*/
+    e.target.previousElementSibling.style.visibility = 'visible';
+  }
+
+  hide(e){
+    /*this.isHidden = true;
+    this.hid ='hidden';*/
+    e.target.style.visibility = 'hidden';
+  }
   constructor() { }
 
   ngOnInit() {
